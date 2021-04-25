2ad8b7fcafcacc31b7a993b147c792919b563831
husinzz <its.husin@gmail.com>
2021-04-25 15:40:15 +0700

    added assignment list

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import { BrowserRouter as Router } from 'react-router-dom';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-import Navbar from './components/navigation.component';
+import Navbar from "./components/navigation.component";
+import AssignmentList from "./components/assign-list.component";
 
 function App() {
   return (
     <Router>
-      <Navbar />
+      <div className="container">
+        <Navbar />
+        <br />
+        <Route path="/" exact component={AssignmentList} />
+      </div>
     </Router>
   );
 }
